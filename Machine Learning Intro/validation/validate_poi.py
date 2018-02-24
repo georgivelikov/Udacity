85eb6706302ca501a101ae8ad31836f28ce27ab5
georgivelikov <gmvelikov@gmail.com>
2018-02-24 23:58:18 +0200

    validation

---
+++ Machine Learning Intro/validation/validate_poi.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+
+"""
+    Starter code for the validation mini-project.
+    The first step toward building your POI identifier!
+
+    Start by loading/formatting the data
+
+    After that, it's not our code anymore--it's yours!
+"""
+
+import pickle
+import sys
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+
+original = "../final_project/final_project_dataset.pkl"
+destination = "../final_project/final_project_dataset_unix.pkl"
+
+content = ''
+outsize = 0
+with open(original, 'rb') as infile:
+    content = infile.read()
+with open(destination, 'wb') as output:
+    for line in content.splitlines():
+        outsize += len(line) + 1
+        output.write(line + str.encode('\n'))
+
+data_dict = pickle.load(open(destination, "rb"), fix_imports=True)
+
+### first element is our labels, any added elements are predictor
+### features. Keep this the same for the mini-project, but you'll
+### have a different feature list when you do the final project.
+features_list = ["poi", "salary"]
+
+original = '../tools/python2_lesson13_keys.pkl'
+destination = '../tools/python2_lesson13_keys_unix.pkl'
+
+content = ''
+outsize = 0
+with open(original, 'rb') as infile:
+    content = infile.read()
+with open(destination, 'wb') as output:
+    for line in content.splitlines():
+        outsize += len(line) + 1
+        output.write(line + str.encode('\n'))
+        
+data = featureFormat(data_dict, features_list, sort_keys = destination)
+labels, features = targetFeatureSplit(data)
+
+
+### it's all yours from here forward!  
+
+from sklearn.tree import DecisionTreeClassifier
+
+clf = DecisionTreeClassifier()
+
+clf.fit(features, labels)
+
+print(clf.score(features, labels))
+
+from sklearn.cross_validation import train_test_split
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+
+clf = DecisionTreeClassifier()
+
+clf.fit(features_train, labels_train)
+
+print(clf.score(features_test, labels_test))
+
+
+
+
